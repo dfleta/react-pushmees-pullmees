a49c3f86ca285448f70102c51addcc3f699ed232
dfleta <gelpiorama@gmail.com>
2022-04-01 17:11:03 +0200

    refactor(App.js): Pasar objeto por data state

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,14 @@ function App() {
   React.useEffect(() => {
     fetch("/box/jerry")
       .then((res) => res.json())
-      .then((box) => setData(box.name))
+      .then((box) => setData(box)) // pasar la propiedad o el objeto
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-         <p>{!data ? "OKNOTOK": data}</p>
+         <p>{!data ? "OKNOTOK": data.name}</p>
       </header>
     </div>
   );

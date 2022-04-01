ca7e3772826edcade34e56895de7ceddfd260dad
dfleta <gelpiorama@gmail.com>
2022-04-01 18:59:47 +0200

    refactor(Meeseeks): Destructuring del objeto meeseeks

--- client/src/components/Meeseeks.js
+++ client/src/components/Meeseeks.js
@@ -3,11 +3,17 @@ import mr_meeseeks from '../images/mr_meeseeks.png'
 
 function Meeseeks(clon) {
 
+    console.log(clon.value);
+
+    let {messageOnCreate, messageOnRequest, _id} = clon.value;
+
+    console.log(messageOnCreate);
+
     return (
         <div className="Meeseeks">
             <header className="Meeseeks-header">
                 <img src={mr_meeseeks} className="App-logo" alt="mr meeseeks" />
-                <p>{!clon.value ? "OKNOTOK" : clon.value.messageOnCreate}</p>
+                <p>{!messageOnCreate ? "OKNOTOK" : messageOnCreate}</p>
             </header>
         </div>
     );

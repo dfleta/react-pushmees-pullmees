51477a461c54ee7fec04e56c3039a5a969ebd745
dfleta <gelpiorama@gmail.com>
2022-04-01 20:34:46 +0200

    feat(PressButton): Implementar evento pressButton
    Crear componentes Meeseeks desde onclick

--- client/src/components/PressButton.js
+++ client/src/components/PressButton.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import Meeseeks from './Meeseeks';
 
 function PressButton() {
 
-    // hook
-    const [data, setData] = React.useState(null);
-
-    React.useEffect(() => {
+    const [meeseeks, setMeeseeks] = useState([]);
+    
+    function createMeeseeks() {
         fetch("/box/pressButton")
             .then((res) => res.json())
-            .then((mr_mees) => setData(mr_mees))
+            .then((mr_mees) => setMeeseeks([...meeseeks, mr_mees]))
             .catch(() => console.log("pressButton NOTOK"))
-    }, [])
+    }
 
     return (
-        <div className="PressButton">
-            <header className="Press-header">
-                <Meeseeks value={data}/>
-                {/* <p>{!data ? "OKNOTOK" : data.messageOnCreate}</p>*/}
-            </header>
-        </div>
+        <div>
+            <button className="PressButton" onClick={createMeeseeks}>
+                Press Button
+                    {/* <Meeseeks value={data}/> */}
+                    {/* <p>{!data ? "OKNOTOK" : data.messageOnCreate}</p>*/}
+            </button>
+            { !meeseeks ? "reality" : meeseeks.map((item) => (<Meeseeks value={item}/> ) ) }
+        </div> 
     );
 }
 

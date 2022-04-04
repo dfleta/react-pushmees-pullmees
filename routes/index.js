ce7cb07d502de37350604c866f34f3a694830765
dfleta <gelpiorama@gmail.com>
2022-04-04 13:55:53 +0200

    refactor(index.pug, index.js): Incluir instrucciones uso app en endpoint api

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'pushmees pullmees' });
+  res.render('index', { title: 'pushmees pullmees', pressButton: "pressButton trae un meeseeks a la existencia al DOM y a MongoAtlas", onClick: 'evento onClic sobre mr meeseeks se lo lleva del DOM y de la base de datos'});
 });
 
 module.exports = router;
